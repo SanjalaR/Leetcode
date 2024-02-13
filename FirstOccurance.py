7e4bbbd595ad4a9e68321bc80aaf913b9a2e0a13
SanjalaR <144262889+SanjalaR@users.noreply.github.com>
2024-02-13 22:56:26 +0530

    Create FirstOccurance.py

---
+++ FirstOccurance.py
@@ -0,0 +1,11 @@
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.find(needle)

0f697818f526e158d954cfc30c68b203d0a3d286
SanjalaR <144262889+SanjalaR@users.noreply.github.com>
2024-02-16 21:25:54 +0530

    Create PlusOne.py

---
+++ PlusOne.py
@@ -0,0 +1,13 @@
+# You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+# Increment the large integer by one and return the resulting array of digits.
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        num = int("".join(map(str, digits)))
+        num+=1
+        res = [int(x) for x in str(num)]
+        return res

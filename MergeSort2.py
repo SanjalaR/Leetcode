bcdfcea04d40c62ce1b0e6eb0014ec9adbbceca4
SanjalaR <144262889+SanjalaR@users.noreply.github.com>
2024-02-21 21:32:20 +0530

    Create MergeSort2.py

---
+++ MergeSort2.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        j = m
+        for i in nums2:
+            nums1[j] = i
+            j+=1
+        nums1.sort()
+
+        

248593ae7a4ffacae1b4247fa1f5cb074c20f157
SanjalaR <144262889+SanjalaR@users.noreply.github.com>
2024-04-19 19:15:38 +0530

    Create GuessGame.py

---
+++ GuessGame.py
@@ -0,0 +1,34 @@
+'''
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+'''
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        low = 1
+        high = n
+        while(low<=high):
+            mid = (low+high)//2
+            g = guess(mid)
+            if g == -1:
+                high = mid-1
+            elif g == 1:
+                low = mid+1
+            else:
+                return mid
+        return mid
